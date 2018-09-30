4f2e117e62fff5ff88e8e659e44f724ae2d3a7ca
shavkunov <mv.shavkunov@yandex.ru>
2018-09-30 22:32:59 +0300

    removed redundant calling

--- hw2/resnext/test.py
+++ hw2/resnext/test.py
@@ -9,9 +9,7 @@ def test_answer():
 
     dataset = TensorDataset(
         torch.rand(2, 3, 224, 224),
-        torch.tensor([0, 1])
+        torch.tensor([0, 1]),
     )
     
     trainer.train(dataset, torch.optim.Adam(net.parameters(), lr=1e-3), torch.nn.CrossEntropyLoss(), epochs=1)
-
-test_answer()
